507aebbb829c1b8ccfff4440edbf42fe76212759
Julia Antokhina <yaantok1@mts.ru>
2021-05-26 20:39:33 +0300

    Source Pipeline features

---
+++ source/pipeline/features.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import argparse
+import yaml
+
+from sklearn.pipeline import Pipeline
+
+from source.input_output import InputLeafData
+from source.features import AddMonth, AddDay, AddHour, DropColumns
+from source.preprocess import DropOuliers
+
+def features(config_path):
+    config = yaml.safe_load(open('params.yaml'))
+    i_o_folder = os.path.join(config['base']['repo_path'], config['load_data']['folder'])
+    
+    station_data = InputLeafData(
+        i_o_folder, 
+        config['load_data']['file']).get_data()
+    
+    # Features Generator
+    features_enineering = [
+        ('add_month', AddMonth()),
+        ('add_day', AddDay()),
+        ('add_hour', AddHour()),
+        # Add here as many feature generating classes as you want
+        ('drop_columns', DropColumns(config['fen']['features_to_drop']))
+    ]
+    
+    # Preprocessing. Different from features generator => class may have fit method.
+    features_preprocessing = []
+    for i in config['preprocessing']['preprocess_outliers']:
+        features_preprocessing.append((i, DropOuliers(i)))
+    
+    features_pipeline = Pipeline(features_enineering + features_preprocessing)
+    
+    station_data = features_pipeline.fit_transform(station_data)
+    station_data.to_csv(os.path.join(i_o_folder, config['output']['features']), index=False)
+
+
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config', dest='config', required=True)
+    args = args_parser.parse_args()
+
+    features(config_path=args.config)
